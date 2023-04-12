c7c7360fd729e26e4fad8da26a344ed08ee7a6b2
ThiagoThe <nitrox2thi@gmail.com>
2023-04-12 13:34:52 -0300

    feat: adicionado fonte e favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
- 
-
   return (
     <div className="App">
-      
+      <h1>Palavra</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
