be0eabda0909ee194b678af48c7f14cdc779f2b1
ThiagoThe <nitrox2thi@gmail.com>
2023-04-15 18:21:12 -0300

    feat: add card na pag medicamentos

--- src/pages/ListaMed.jsx
+++ src/pages/ListaMed.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "../components/Header/Header";
 import { Container, Row } from "react-bootstrap";
+import { CardMed } from "../components/CardMed";
 
 function ListaMed() {
   return (
@@ -9,6 +10,9 @@ function ListaMed() {
       <Container>
         <Row className="m-5 text-center">
           <h2>Medicamentos</h2>
+        </Row>
+        <Row>
+          <CardMed />
         </Row>
       </Container>
     </>
