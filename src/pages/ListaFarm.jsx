abadfb29ed1afdd02594ccd48985caf124b20eac
ThiagoThe <nitrox2thi@gmail.com>
2023-04-16 17:37:43 -0300

    feat: criado componente mapa

--- src/pages/ListaFarm.jsx
+++ src/pages/ListaFarm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "../components/Header/Header";
 import { Container, Row } from "react-bootstrap";
+import { Map } from "../components/Map/Map";
 
 function ListaFarm() {
   return (
@@ -9,6 +10,9 @@ function ListaFarm() {
       <Container>
         <Row className="m-5 text-center">
           <h2>Farmácias cadastradas</h2>
+        </Row>
+        <Row>
+          <Map />
         </Row>
       </Container>
     </>
