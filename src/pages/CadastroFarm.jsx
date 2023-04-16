55b0ab9e11e40d182bfd8fc2223f948cab1fc6bf
ThiagoThe <nitrox2thi@gmail.com>
2023-04-16 09:25:29 -0300

    feat: add busca por cep na pag cadastro farm

--- src/pages/CadastroFarm.jsx
+++ src/pages/CadastroFarm.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Header } from "../components/Header/Header";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import { Container } from "react-bootstrap";
+import { Container, Button, Col, Form, Row, InputGroup } from "react-bootstrap";
 
 function CadastroFarm() {
   const [endereco, setEndereco] = useState();
@@ -100,18 +96,21 @@ function CadastroFarm() {
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridCep">
               <Form.Label>CEP</Form.Label>
-              <Form.Control
-                value={formulario.cep}
-                onChange={(evento) =>
-                  atualizarCampo("cep", evento.target.value)
-                }
-                required
-              />
-              <Button variant="primary" onClick={buscarCep}>
-                B
-              </Button>
+              <InputGroup>
+                <Form.Control
+                  value={formulario.cep}
+                  onChange={(evento) =>
+                    atualizarCampo("cep", evento.target.value)
+                  }
+                  required
+                />
+
+                <Button variant="primary" onClick={buscarCep}>
+                  Buscar
+                </Button>
+              </InputGroup>
             </Form.Group>
-            {console.log(endereco)}
+
             <Form.Group as={Col} controlId="formGridCidade">
               <Form.Label>Cidade</Form.Label>
               <Form.Control type="text" value={endereco?.localidade} required />
