58e71d2676fe4e71b5cc593cd9f511926bfc2c88
ThiagoThe <nitrox2thi@gmail.com>
2023-04-22 12:53:29 -0300

    refactor: excluido funcionalidades invalidas

--- src/components/Grids/MedGrid.jsx
+++ src/components/Grids/MedGrid.jsx
@@ -2,16 +2,14 @@ import { useMed } from "../../contexts/medContext";
 import { CardMed } from "../Card/CardMed";
 import { Col, Row } from "react-bootstrap";
 
-export const MedGrid = ({ medicamentos }) => {
+export const MedGrid = ({ medicamento }) => {
   const context = useMed();
-
-  console.log(context.medicamentos);
 
   return (
     <Row xs={1} md={3} className="g-4">
       {context.medicamentos.length ? (
         context.medicamentos.map((medicamento) => (
-          <Col key={medicamento.id}>
+          <Col key={medicamento.nome}>
             <CardMed medicamento={medicamento} />
           </Col>
         ))
--- src/pages/ListaMed.jsx
+++ src/pages/ListaMed.jsx
@@ -2,28 +2,19 @@ import { React, useState } from "react";
 import { Header } from "../components/Header/Header";
 import { Container, Row } from "react-bootstrap";
 import Footer from "../components/Footer/Footer";
-import NavSearch from "../components/Search/NavSearch";
 import { MedGrid } from "../components/Grids/MedGrid";
-import { useMed } from "../contexts/medContext";
 
 function ListaMed() {
-  const [buscar, setBuscar] = useState("");
-  const context = useMed();
-  console.log(context.medicamentos);
   return (
     <>
       <Header />
-      <NavSearch buscar={buscar} setBuscar={setBuscar} />
+
       <Container>
         <Row className="m-5 text-center">
           <h2>Medicamentos</h2>
         </Row>
         <Row>
-          <MedGrid
-            medicamentos={context.medicamentos.filter((medicamento) =>
-              medicamento.nome.toLowerCase().includes(buscar.toLowerCase())
-            )}
-          />
+          <MedGrid />
         </Row>
       </Container>
       <Footer />
