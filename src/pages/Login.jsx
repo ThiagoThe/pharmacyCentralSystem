d6c764197ed5b6ad585ebe9cf57cf97ebc43d773
ThiagoThe <nitrox2thi@gmail.com>
2023-04-22 22:04:03 -0300

    feat: add img de bg login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { Container, Form, Button, Alert, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import "../index.css";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -27,40 +28,42 @@ function Login() {
   };
 
   return (
-    <Container className="d-flex flex-column align-items-center justify-content-center vh-100 bg-white ">
-      <Image src="/img/logo1.png" width="200" className=""></Image>
-      <Form className="d-flex flex-column align-items-center justify-content-center p-4 mb-4 mt-4 bg-white rounded shadow">
-        <p>Para entrar faça seu Login</p>
-        <Form.Control
-          className="w-100 mb-4"
-          type="email"
-          placeholder="Seu email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        ></Form.Control>
-        <Form.Control
-          className="w-100 mb-4"
-          type="password"
-          placeholder="Sua senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></Form.Control>
-        <Button
-          className="w-100"
-          variant="primary"
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Fazer Login
-        </Button>
-        {validPassword && (
-          <Alert variant="danger" className="w-100 mt-4">
-            A senha deve ter no minímo 8 caracteres, com pelo menos uma letra e
-            um número
-          </Alert>
-        )}
-      </Form>
-    </Container>
+    <div className="bg-login">
+      <Container className=" d-flex flex-column align-items-center justify-content-center vh-100 ">
+        <Image src="/img/logo1.png" width="200" className=""></Image>
+        <Form className="d-flex flex-column align-items-center justify-content-center p-4 mb-4 mt-4 bg-white rounded shadow">
+          <p>Para entrar faça seu Login</p>
+          <Form.Control
+            className="w-100 mb-4"
+            type="email"
+            placeholder="Seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+          <Form.Control
+            className="w-100 mb-4"
+            type="password"
+            placeholder="Sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+          <Button
+            className="w-100"
+            variant="primary"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Fazer Login
+          </Button>
+          {validPassword && (
+            <Alert variant="danger" className="w-100 mt-4">
+              A senha deve ter no minímo 8 caracteres, com pelo menos uma letra
+              e um número
+            </Alert>
+          )}
+        </Form>
+      </Container>
+    </div>
   );
 }
 
